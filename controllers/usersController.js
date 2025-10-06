e1d15263b4b943a0704233c141cbd010cf7afa6d
joao-domingos <joaopedro.mdomingos@gmail.com>
2025-10-06 02:36:33 -0300

    finishing user controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -46,4 +46,59 @@ async function buscarNomeUsuario(nome) {
 	}
 }
 
+async function buscarEmailUsuario(email) {
+	if (!email) {
+		throw new Error("campo email obrigatorio");
+	}
+	if (!email.includes("@") {
+		throw new Error("email invalido");
+	}
+	try {
+		const userByEmail = await User.buscarEmail(email);
+		
+		if (!userByEmail) {
+			console.log("email nao cadastrado no sistema");
+			return;
+		}
+		if (userByName.length > 1) {
+			throw new Error("email ja cadastro no sistema")
+		}
+
+		console.log(`buscando:  ${email}\n`);
+		console.log(`name: ${userByEmail.name} email: ${userByEmail.email}`);
+	}
+	catch (error) {
+		console.log("erro ao buscar usuario pelo email");
+		logger(error);
+	}
+}
+
+async function deletarUsuario(name, email) {
+	if (!name || !email) {
+		throw new Error("os dois campos sao obrigatorios");
+	}
+	if (!email.includes("@")) {
+		throw new Error("email invalido")
+	}
+	if (typeof name !== "string") {
+		throw new Error("nome invalido");
+	}
+
+	const toExclude = User.buscarEmail(email);
+
+	if (!toExclude) {
+		console.log("nao foi possivel deletar usuario, nao presente no sistema");
+	}
+
+	try {
+		await toExclude.deletar(name, email);
+		console.log(`usuario ${email} deletado com sucesso`);
+	}
+	catch (error) {
+		console.log("erro ao deletar usuario");
+		logger(error);
+	}
+}
+
+module.exports = { cadastrarUsuario, buscarNomeUsuario, buscarEmailUsuario, deletarUsuario };
 
