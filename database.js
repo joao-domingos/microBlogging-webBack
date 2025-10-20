f168c81d8db567a3353bdcab61ef2845048f4c54
joao-domingos <joaopedro.mdomingos@gmail.com>
2025-10-20 15:27:23 -0300

    exclusão arquivo desnecessário e ultimos ajustes readme

--- database.js
+++ database.js
@@ -22,4 +22,18 @@ function getDB() {
 	return db;
 }
 
+//código presente no meu arquivo excluído de criação de server
+/*const http = require('http');
+const { database } = require('./database');
+
+const PORT = 8080;
+
+const server = http.createServer((req, res) => {
+	res.end("entrega1-webBack")
+});
+
+server.listen(PORT, () => {
+    console.log(`servidor rodando em http://localhost:${PORT}`);
+});*/
+
 module.exports = { connectDB, desconnectDB, getDB };
