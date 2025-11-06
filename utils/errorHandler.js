1cffc390f18fcc6034df96c4d9293eb4f873375f
joao-domingos <joaopedro.mdomingos@gmail.com>
2025-11-06 16:29:22 -0300

    apagando estrutura de projeto para usar o express generator

--- utils/errorHandler.js
+++
@@ -1,6 +0,0 @@
-function handleError(res, err) {
-	res.writeHead(500, { 'Content-Type': 'application/json' });
-	res.end(JSON.stringify({ error: 'erro interno do servidor', details: err.message }));
-}
-
-module.exports = { handleError }
